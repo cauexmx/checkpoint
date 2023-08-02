99da99c08e723ca76bbd1241138336b9088517f3
cauexmx <127966350+cauexmx@users.noreply.github.com>
2023-08-02 10:45:42 -0300

    Add files via upload

---
+++ checkpoint.js
@@ -0,0 +1,33 @@
+var favoriteFood = 'pizza';
+var numOsSlices = 8;
+console.log(favoriteFood);
+console.log(numOsSlices);
+let changeMe = true;
+console.log(changeMe);
+const entree = 'Enchiladas';
+console.log(entree);
+/* entree = 'Pois'*/
+levelUp = 10;
+levelUp += 5;
+console.log(levelUp);
+powerLevel = 150;
+powerLevel -= 100;
+console.log(powerLevel)
+multiplyMe = 11;
+multiplyMe *= 10;
+console.log(multiplyMe);
+quarterMe = 100;
+quarterMe /= 4;
+console.log(quarterMe)
+gainedDollar = 2
+console.log(++gainedDollar)
+lostDollar = 5;
+console.log(--lostDollar)
+myName = 'Cauê';
+myCity = 'Osasco';
+console.log(`My name is ${myName}. My favorite city is ${myCity}`)
+let Myvariable = 'teste'
+console.log(typeof Myvariable);
+
+
+
